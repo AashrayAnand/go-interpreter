d00118727a15faa53e65c72c2cfe86aaff4d1af9
Aashray <aashray@uw.edu>
2019-02-26 14:30:24 -0800

    implemented simple struct to represent tokens, constants for simplex token types (parentheses, semis, assignments, functions etc.), implemented a simple lexer struct to process tokens, with a NextToken function, and readChar, readIdentifier, and readInt methods for the lexer

---
+++ lexer/lexer.go
@@ -0,0 +1,119 @@
+package lexer
+
+import (
+	"go-interpreter/token"
+)
+
+// Lexer : lexer struct
+type Lexer struct {
+	input        string
+	position     int  // current position in input (points to char)
+	readPosition int  // current reading position in input (after current char)
+	ch           byte // current char under examination
+
+	// position is what we just read, readPosition is what we will read nex
+}
+
+// New : create new lexer
+func New(input string) *Lexer {
+	l := &Lexer{input: input}
+	l.readChar()
+	return l
+}
+
+// NextToken : get the next token
+func (l *Lexer) NextToken() token.Token {
+	var tok token.Token
+
+	l.skipWhiteSpace()
+
+	switch l.ch {
+	case '=':
+		tok = newToken(token.ASSIGN, l.ch)
+	case ';':
+		tok = newToken(token.SEMICOLON, l.ch)
+	case '(':
+		tok = newToken(token.LPAREN, l.ch)
+	case ')':
+		tok = newToken(token.RPAREN, l.ch)
+	case ',':
+		tok = newToken(token.COMMA, l.ch)
+	case '+':
+		tok = newToken(token.PLUS, l.ch)
+	case '{':
+		tok = newToken(token.LBRACE, l.ch)
+	case '}':
+		tok = newToken(token.RBRACE, l.ch)
+	case 0:
+		tok.Literal = ""
+		tok.Type = token.EOF
+	// if it does not match any special character
+	// check if it is a literal
+	default:
+		if isLetter(l.ch) {
+			tok.Literal = l.readIdentifier()
+			tok.Type = token.LookupIdent(tok.Literal)
+			return tok
+		} else if isNumber(l.ch) {
+			tok.Literal = l.readNumber()
+			tok.Type = token.INT
+			return tok
+		} else {
+			tok = newToken(token.ILLEGAL, l.ch)
+		}
+	}
+	l.readChar()
+	return tok
+}
+
+// newToken : create new token of type t, and with literal value v
+func newToken(t token.TokenType, v byte) token.Token {
+	return token.Token{Type: t, Literal: string(v)}
+}
+
+// returns whether a character is a letter, or underscore
+func isLetter(ch byte) bool {
+	return 'a' <= ch && ch <= 'z' || 'A' <= ch && ch <= 'Z' || ch == '_'
+}
+
+func isNumber(ch byte) bool {
+	return '0' <= ch && ch <= '9'
+}
+
+// readIdentifier :
+func (l *Lexer) readIdentifier() string {
+	position := l.position
+	// increment position pointer while the
+	// character is still a letter
+	for isLetter(l.ch) {
+		l.readChar()
+	}
+	return l.input[position:l.position]
+}
+
+func (l *Lexer) readNumber() string {
+	position := l.position
+	for isNumber(l.ch) {
+		l.readChar()
+	}
+	return l.input[position:l.position]
+}
+
+// skipWhiteSpace : lexer method which skips whitespace
+func (l *Lexer) skipWhiteSpace() {
+	// keep reading characters until we skip through white space
+	for l.ch == ' ' || l.ch == '\t' || l.ch == '\n' || l.ch == '\r' {
+		l.readChar()
+	}
+}
+
+// readChar : read next character
+func (l *Lexer) readChar() {
+	if l.readPosition >= len(l.input) {
+		l.ch = 0
+	} else {
+		l.ch = l.input[l.readPosition]
+	}
+	l.position = l.readPosition
+	l.readPosition++
+}
---
+++ token/token.go
@@ -0,0 +1,48 @@
+package token
+
+type TokenType string
+
+// every token consists of type, and literal value
+type Token struct {
+	Type    TokenType
+	Literal string
+}
+
+const (
+	ILLEGAL = "ILLEGAL"
+	EOF     = "EOF"
+
+	// identifiers + literals
+	IDENT = "IDENT" // add, x, y
+	INT   = "INT"   // 0..9
+
+	// operators
+	ASSIGN = "="
+	PLUS   = "+"
+
+	// delimiters
+	COMMA     = ","
+	SEMICOLON = ";"
+
+	LPAREN = "("
+	RPAREN = ")"
+	LBRACE = "{"
+	RBRACE = "}"
+
+	// keywords
+	LET      = "LET"
+	FUNCTION = "FUNCTION"
+)
+
+var keywords = map[string]TokenType{
+	"fn":  FUNCTION,
+	"let": LET,
+}
+
+func LookupIdent(ident string) TokenType {
+	if tok, ok := keywords[ident]; ok {
+		return tok
+	}
+
+	return IDENT
+}
